29f41f4c1ca9d4f343d1ec3dc641d3005be4e77d
Jaime Daniel Corrêa Mendes <jaimedaniel@tartech.com.br>
2025-10-21 12:25:18 -0300

    Changed lib versions

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,11 +8,13 @@ rm poetry.lock
 rm ./dist/*.whl
 rm ./dist/*.gz
 
-python3 -m virtualenv venv
+python3 -m venv venv
 
 source venv/bin/activate
 
 pip install poetry
+poetry self add poetry-plugin-export
+poetry export -f requirements.txt --output requirements.txt --without-hashes
 poetry install
 poetry build
 
